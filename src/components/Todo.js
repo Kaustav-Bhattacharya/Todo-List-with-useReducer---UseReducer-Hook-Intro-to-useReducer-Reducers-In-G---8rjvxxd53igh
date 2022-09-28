39d3db2cfd02b335f873d4e73da1c0daf82d21cf
Kaustav-Bhattacharya <bhattacharyar64@gmail.com>
2022-09-29 03:11:59 +0530

    complpeted

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,16 @@
-import React from 'react';
-const Todo = () => {
-    return (
-        <></>
-    )
-}
+import React from "react";
+const Todo = ({ id, value, dispatch }) => {
+  return (
+    <>
+      <div className="todo-title">{value}</div>
+      <button
+        className="todo-delete"
+        onClick={() => dispatch({ type: "delete_todo", payload: { id: id } })}
+      >
+        Delete
+      </button>
+    </>
+  );
+};
 
-
-export { Todo }
-
+export { Todo };
